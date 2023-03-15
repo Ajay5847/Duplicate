878eeaaaf2053c0573758458547009dfc8bc8268
Ajay5847 <93735050+Ajay5847@users.noreply.github.com>
2023-03-15 21:00:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from './components/Home/Home';
+import { Routes, Route } from 'react-router-dom'
+import Show from './components/Show/Show';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/show' element={<Show/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
